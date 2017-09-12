7a16fceed334d0f91e1e761e6790481462db2ecc
LineageOS-MTK <luzejij@binka.me>
2017-09-13 00:47:13 +0300

    fix

--- libmtk_symbols/mtk_wmv.cpp
+++ libmtk_symbols/mtk_wmv.cpp
@@ -14,5 +14,3 @@ extern "C" void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
 extern "C" void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
        return _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
     }
-
-}
