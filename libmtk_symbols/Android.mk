6e679d9f16a43384925cb1b901a035bdb03a796a
LineageOS-MTK <luzejij@binka.me>
2017-09-12 15:24:18 +0300

    Fix

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -7,17 +21,8 @@ LOCAL_SRC_FILES := \
     mtk_ril.cpp \
     icu55.c
 
-LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
-                          libicuuc libicui18n libcrypto libstagefright_foundation
+LOCAL_SHARED_LIBRARIES := libbinder
 LOCAL_MODULE := libmtk_symbols
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_CLANG := true
-LOCAL_CPPFLAGS := -std=c++1y
-LOCAL_CPPFLAGS += -Wno-exit-time-destructors
-LOCAL_CPPFLAGS += -Wno-global-constructors
-LOCAL_CPPFLAGS += -Wno-c++98-compat-pedantic
-LOCAL_CPPFLAGS += -Wno-four-char-constants
-LOCAL_CPPFLAGS += -Wno-padded
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
