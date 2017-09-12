066fe3a95a0f7e70050d7cff5abf79ee880ed8fe
LineageOS-MTK <luzejij@binka.me>
2017-09-13 00:53:36 +0300

    fix

--- libmtk_symbols/mtk_wmv.cpp
+++ libmtk_symbols/mtk_wmv.cpp
@@ -1,16 +1,11 @@
 #include <stdint.h>
 
-extern "C" int _ZN7android16MediaBufferGroupC1Ej(unsigned int);
-extern "C" int _ZN7android16MediaBufferGroupC1Ev() {
-    return _ZN7android16MediaBufferGroupC1Ej(0);
+extern int _ZN7android16MediaBufferGroupC1Ej(void **buffer, bool nonBlocking);
+int _ZN7android16MediaBufferGroupC1Ev(void **buffer) {
+    return _ZN7android16MediaBufferGroupC1Ej(buffer, false);
 }
 
-extern "C" bool _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb();
-extern "C" bool _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb() {
-  return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb();
+extern int _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(void **buffer, bool nonBlocking);
+int _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferE(void **buffer) {
+    return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(buffer, false);
 }
-extern "C" void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
-
-extern "C" void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
-       return _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
-    }
