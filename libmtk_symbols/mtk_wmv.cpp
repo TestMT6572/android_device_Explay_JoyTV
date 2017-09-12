217b4b0d526b1dec4d623cf9f0055420738dc0e7
LineageOS-MTK <luzejij@binka.me>
2017-09-13 00:04:05 +0300

    Fix

--- libmtk_symbols/mtk_wmv.cpp
+++ libmtk_symbols/mtk_wmv.cpp
@@ -6,18 +6,18 @@
 #include <media/IMediaSource.h>
 
 extern "C" {
-int _ZN7android16MediaBufferGroupC1Ej(void **buffer, bool nonBlocking);
-int _ZN7android16MediaBufferGroupC1Ev(void **buffer) {
-    return _ZN7android16MediaBufferGroupC1Ej(buffer, false);
-}
+int _ZN7android16MediaBufferGroupC1Ej(unsigned int);
+    int _ZN7android16MediaBufferGroupC1Ev() {
+        return _ZN7android16MediaBufferGroupC1Ej(0);
+    }
 
 void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(
 	    int64_t *time_us, int32_t *mode) {
      _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(time_us, mode);
 }
 
-int _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(void **buffer, bool nonBlocking);
-int _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferE(void **buffer) {
-    return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(buffer, false);
-}
+int _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(unsigned int);
+    int _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferE() {
+        return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(0);
+    }
 }
