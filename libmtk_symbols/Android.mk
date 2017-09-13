73e6ea5fe068ef4db0c98fbbb77f8a5e89439712
LineageOS-MTK <luzejij@binka.me>
2017-09-13 16:45:17 +0300

    Fix

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -18,9 +18,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     mtk_ui.cpp \
     mtk_ril.cpp \
-    icu55.c
+    icu51.c
 
-LOCAL_SHARED_LIBRARIES := libbinder liblog libicuuc libui
+LOCAL_SHARED_LIBRARIES := libbinder liblog libicuuc libui \
+                          libstagefright
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
---
+++ libmtk_symbols/mtk_wmv.cpp
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <media/IMediaSource.h>
+#include <sys/types.h>
+
+extern "C" {
+extern int32_t _ZN7android16MediaBufferGroupC1Ej(void*);
+int32_t _ZN7android16MediaBufferGroupC1Ev(void* ptr) {
+    return _ZN7android16MediaBufferGroupC1Ej(ptr);
+}
+}
+
+extern "C" {
+void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(
+	    int64_t *time_us, int32_t *mode) {
+     _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(time_us, mode);
+}
+}
+
+extern "C" {
+extern int32_t _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(void*);
+int32_t _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferE(void* ptr) {
+    return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb(ptr);
+}
+}
+
