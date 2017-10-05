9d4ebca28784070031d786bdc5983abd0558556e
tribetmen <dima.koloskov.1997@yandex.ru>
2017-10-05 15:08:27 +0300

    Revert "Fix invalid number error"
    Fix not work

--- ril/telephony/java/com/android/internal/telephony/MediaTekRIL.java
+++ ril/telephony/java/com/android/internal/telephony/MediaTekRIL.java
@@ -1026,24 +1026,6 @@ public class MediaTekRIL extends RIL implements CommandsInterface {
 	}
     }
     
-    protected Object
-    responseFailCause(Parcel p) {
-        int numInts;
-        int response[];
-
-        numInts = p.readInt();
-        response = new int[numInts];
-        for (int i = 0 ; i < numInts ; i++) {
-            response[i] = p.readInt();
-        }
-        LastCallFailCause failCause = new LastCallFailCause();
-        failCause.causeCode = response[0];
-        if (p.dataAvail() > 0) {
-          failCause.vendorCause = p.readString();
-        }
-        return failCause;
-    }
- 
     public void setDataAllowed(boolean allowed, Message result) {
         handle3GSwitch();
 
